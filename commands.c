4d78100f70dc4ffcf34f43bd1364bca0de958e0d
Andrew Han <awhan@wpi.edu>
2014-11-05 15:35:41 -0500

    Create gitignore

--- commands.c
+++ commands.c
@@ -3,6 +3,13 @@
 #include <unistd.h>
 #include <string.h>
 
+#define MSGLENGTH 7
+
+void sendMessage(char cid){
+	void* messagePtr = malloc(sizeof(char) * MSGLENGTH);
+	memcpy(&cid, messagePtr, 1);
+}
+
 void requestResponseFromHost(char** cmdArgs){
 	printf("hi");
 }
